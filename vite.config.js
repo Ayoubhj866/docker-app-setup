cb8589dbf43573848c96e9d48cb932b555826bc6
Ayoubhj866 <ayoubelhajji866@gmail.com>
2024-07-07 13:43:33 +0100

    setup flowbite + breez + some change on local domaine name for my app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'myapp.com',
+            port: 5173,
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        // tailwindcss(),
     ],
+
 });
